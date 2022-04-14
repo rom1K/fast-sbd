451df09ce37d739c92238f84879c8727db477947
Romain Keramitas <romain.keramitas@preligens.com>
2022-04-14 17:21:03 +0200

    Add setup script
    Signed-off-by: Romain Keramitas <romain.keramitas@preligens.com>

---
+++ scripts/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# TIP: http://redsymbol.net/articles/unofficial-bash-strict-mode
+set -euo pipefail
+IFS=$'\n\t'
+
+CYAN="\e[1;96m"
+GREEN="\e[1;92m"
+RESET="\e[0m"
+
+echo -e "${GREEN}Creating a virtualenv ${CYAN}${PROJECT_NAME}${GREEN} with Python ${CYAN}${PYTHON_VERSION}${RESET}"
+
+pyenv install ${PYTHON_VERSION} --skip-existing
+pyenv virtualenv ${PYTHON_VERSION} ${PROJECT_NAME}
+pyenv local ${PROJECT_NAME}
+
+echo -e "${GREEN}Updating pip, setuptools & wheel${RESET}"
+pip install --upgrade pip setuptools wheel
+
+echo -e "${GREEN}Installing dev requirements${RESET}"
+pip install -r requirements-dev.txt
+
+echo -e "${GREEN}Setting up pre-commit${RESET}"
+pre-commit install
+
+echo -e "${GREEN}All set !${RESET}"
